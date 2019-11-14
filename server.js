2492b8044239a6c74a854a91f29560df63be4ad2
kim <s158614@nishitech.ac.jp>
2019-11-14 12:56:02 +0900

    req, res using

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 
-const post =  1234;
+const port =  1234;
 
-app.listen(console.log("서버를 시작합니다."));
+app.listen(port, console.log("서버를 시작합니다."));
+
+
+
+app.use((req, res) => {
+    res.json({
+        msg : "홈페이지에 오신걸 환영합니다."
+    })
+});
